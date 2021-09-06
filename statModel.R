25aaab04f07e1cf3cd217ae40a38cb439edd1366
cankiziloz <49860352+cankiziloz@users.noreply.github.com>
2021-09-06 17:57:36 +0300

    Create statModel.R

---
+++ statModel.R
@@ -0,0 +1,37 @@
+data(iris)
+
+head(iris)
+
+#Creating a scatterplot
+plot(
+  x = iris$Petal.Length,
+  y = iris$Petal.Width,
+  main = "Iris Petal Length vs. Width",
+  xlab = "Petal Length",
+  ylab = "Petal width")
+
+#Creating a linear regression model
+model <- lm(
+  formula = Petal.Width ~ Petal.Length,
+  data = iris)
+
+#Drawing the line on the plot
+lines(
+  x = iris$Petal.Length,
+  y = model$fitted.values,
+  col = "red",
+  lwd = 3
+)
+
+#Getting correlation coefficient
+cor(x= iris$Petal.Length,
+    y = iris$Petal.Width)
+
+#Predicting new values according to the model 
+#This function predicts the petal width values for the given values relatively
+predict(
+  object = model,
+  newdata = data.frame(
+    Petal.Length = c(1 , 2, 3)
+  )
+)
